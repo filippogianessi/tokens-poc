c30d40ae06d812b480463741b99b52c310a492b6
Filippo Gianessi <gianessi.filippo@gmail.com>
2024-06-05 19:02:10 +0200

    added js

---
+++ build/js/variables.js
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Jun 2024 16:55:12 GMT
+ */
+
+export const GlobalColorDelightBlue200 = "#b3dbff";
+export const GlobalColorDelightBlue300 = "#80c4ff";
+export const GlobalColorDelightBlue500 = "#0088ff";
+export const GlobalColorDelightBlue600 = "#0075ea";
+export const GlobalColorDelightNeutral0 = "#ffffff";
+export const GlobalColorDelightNeutral950 = "#111111";
+export const GlobalColorLegacyBlue200 = "#c0e5fc";
+export const GlobalColorLegacyBlue300 = "#96d4f8";
+export const GlobalColorLegacyBlue500 = "#25a1e4";
+export const GlobalColorLegacyBlue600 = "#0589d6";
+export const GlobalColorLegacyNeutral0 = "#ffffff";
+export const GlobalColorLegacyNeutral950 = "#2f3a42";
+export const GlobalColorDummyBlue200 = "#c0cafc";
+export const GlobalColorDummyBlue300 = "#96b1f8";
+export const GlobalColorDummyBlue500 = "#7278f5";
+export const GlobalColorDummyBlue600 = "#2b48d0";
+export const GlobalColorFunctionalBrandPrimary = "#0088ff";
+export const GlobalColorFunctionalTextInvert = "#ffffff";
+export const GlobalColorButtonPrimaryBackgroundDefault = "#0088ff";
+export const GlobalColorButtonPrimaryForegroundDefault = "#ffffff";
